16998184dfd052a2a0561387a17dc85d01bfe6b6
Michal Krawczykowski <mkrawczykowski1@sii.pl>
2021-11-19 17:52:57 +0100

    arraye i fory

--- script.js
+++ script.js
@@ -12,3 +12,22 @@ console.log(zmienna2);
 console.log(zmienna1+zmienna3);
 console.log(zmienna2+zmienna4);
 console.log(zmienna2+' '+zmienna4);
+
+
+
+console.log('======================');
+console.log('=====  ARRAYE  ======');
+console.log('======================');
+
+
+var owoce = ['Jabłko', 'Banan', '', '', ''];
+console.log(owoce[0]);
+console.log(zmienna2[0]); // traktujemy stringa jak tablicę = można wyświetlić jedną literę 
+
+console.log('======================');
+console.log('=====    FOR    ======');
+console.log('======================');
+
+for (var i = 0; i < 9; i++) {
+    console.log('index: ' + i);
+}
