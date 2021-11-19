29b8cd9ee1b90802cdabb8b1430e69a269b0b075
Michal Krawczykowski <mkrawczykowski1@sii.pl>
2021-11-19 17:27:29 +0100

    variables and console.log

---
+++ script.js
@@ -0,0 +1,14 @@
+console.log('Artur'); //wyświetlanie w konsoli - Zbadaj -> karta Console
+console.log('======================');
+
+
+var zmienna1 = 1; //zmienna liczbowa
+var zmienna2 = 'Artur'; //zmienna tekstowa, string
+var zmienna3 = 4;
+var zmienna4 = 'Zatoń';
+
+console.log(zmienna1);
+console.log(zmienna2);
+console.log(zmienna1+zmienna3);
+console.log(zmienna2+zmienna4);
+console.log(zmienna2+' '+zmienna4);
